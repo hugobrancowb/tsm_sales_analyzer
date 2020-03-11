1e2bdaf78827df3f8f1191bc2ae2ba86bd9d3abb
Hugo <hugobrancowb@gmail.com>
2020-03-11 14:47:57 -0300

    nova função que retorna tamanho da lista de meses

--- src/calendar/ListOfMonths.java
+++ src/calendar/ListOfMonths.java
@@ -13,22 +13,25 @@ public class ListOfMonths {
 		max.set(Calendar.DAY_OF_MONTH, 1);
 		min.set(Calendar.DAY_OF_MONTH, 1);
 		
-		System.out.println("maxTime = " + max.getTime());
-		System.out.println("minTime = " + min.getTime());
+		Calendar index = (Calendar)min.clone();
 		
-		Calendar index = (Calendar)min.clone();;
-		
-		while((index.get(Calendar.MONTH) != max.get(Calendar.MONTH)) || (index.get(Calendar.YEAR) != max.get(Calendar.YEAR))) {
+		while((index.get(Calendar.MONTH) != max.get(Calendar.MONTH)) ||
+              (index.get(Calendar.YEAR) != max.get(Calendar.YEAR))) {
 			
 			Calendar eachmonth = (Calendar)index.clone();			
 			listcalendar.add(eachmonth);
 			index.add(Calendar.MONTH, 1);
 		}
-		listcalendar.add(index); /* add the last month */
+		
+		listcalendar.add(index); /* adds the last month */
 	}
 
 	private ArrayList<Calendar> getListcalendar() {
 		return listcalendar;
+	}
+	
+	public int getSize() {
+		return listcalendar.size();
 	}
 
 	public int getMonth(int i) {
